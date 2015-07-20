2da76dcb15a8e67e08acc65d7326d8199599ad72
Buffest <buffestoverflow@gmail.com>
2015-07-20 13:35:30 +0800

    first commit

---
+++ src/api/util/Signup.java
@@ -0,0 +1,60 @@
+package api.util;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DbUtil;
+
+/**
+ * Servlet implementation class Signup
+ */
+@WebServlet("/api/util/signup")
+public class Signup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Signup() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("123");
+		
+		DbUtil dbUtil = new DbUtil();
+		try {
+			Connection conn = dbUtil.getConnection();
+			Statement stmt = conn.createStatement();
+			String sql = "SELECT * FROM huiwan.account";
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				System.out.println(rs.getString(2));
+			}
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
